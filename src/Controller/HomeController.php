1c8d9667c9a169c1017b171e48f116824524bfbf
JS.Derouet <js.derouet@sfr.fr>
2022-06-30 05:31:22 +0200

    add dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,15 @@ class HomeController extends AbstractController
     public function index():Response 
     {
         return $this->render('home/index.html.twig', [
+
+        ]);
+    }
+
+
+    #[Route('/user', name: 'user_index', methods:['GET'])]
+    public function showUser():Response
+    {
+        return $this->render('user/index.html.twig', [
 
         ]);
     }
