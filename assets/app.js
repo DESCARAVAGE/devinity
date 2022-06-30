82cfff9e54b4678150d8e8e1f80c6e5821968320
JS.Derouet <js.derouet@sfr.fr>
2022-06-30 10:47:06 +0200

    add favicon

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import logoPath from './images/logo.png';
+let html = `<img src="${logoPath}" alt="logo">`;
 
 require('bootstrap');
 // start the Stimulus application
 import './bootstrap';
-console.log('cococ');
